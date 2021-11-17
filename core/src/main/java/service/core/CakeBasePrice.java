b751466371eba972570a30d2106049a25dbfe846
shijieliu789 <shijie.liu@ucdconnect.ie>
2021-11-17 22:56:39 +0000

    Cake type and cake icing prices.

--- core/src/main/java/service/core/CakeBasePrice.java
+++
@@ -1,5 +0,0 @@
-package service.core;
-
-public interface CakeBasePrice {
-
-}
--- core/src/main/java/service/core/IcingPrice.java
+++ core/src/main/java/service/core/IcingPrice.java
@@ -1,4 +1,10 @@
 package service.core;
 
 public interface IcingPrice {
+    public static final int FONDANT = 5;
+    public static final int BUTTERCREAM = 7;
+    public static final int WHIPPED_CREAM = 6;
+    public static final int ROYAL_ICING = 10;
+    public static final int CREAM_CHEESE_FROST = 7;
+    public static final int MERINGUE = 8;
 }
